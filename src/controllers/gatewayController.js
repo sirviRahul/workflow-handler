6c28e559a7368f7562edd6db5e75a5fc4ce41d62
RAHULSIRVI1101 <rahulsirvi1101@gmail.com>
2024-11-20 18:25:13 +0530

    fix

---
+++ src/controllers/gatewayController.js
@@ -0,0 +1,10 @@
+const { log } = require("console")
+
+const handler = (event) => {
+log.info('the event is ',event);
+const {httpMethod , resource} = event;
+try{
+    const router = {
+    }
+}
+}
